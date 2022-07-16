a6b075378b4878629d3a1b077a8d147bdef348d8
Vadim Saprykin <sprkweb@ya.ru>
2022-07-16 15:42:32 +0300

    Update `init` documentation

--- cmd/init.go
+++ cmd/init.go
@@ -15,12 +15,12 @@ var initCmd = &cobra.Command{
 	Long: `Initialize an empty financial plan
 
 Example:
-Initialize an empty plan for 15 weeks ahead with the calculation interval of 3 weeks:
+Initialize an empty plan for 15 weeks ahead
+with the calculation interval of 3 weeks:
 $ finaplan init --each 3 --weeks --intervals 5
 ---
 interval_type: weeks
 interval_length: 3
-interval_amount: 5
 ---
 0
 0
