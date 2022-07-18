a567f57a247be3e7789bc22ea0c348fd3e4017b2
Vadim Saprykin <sprkweb@ya.ru>
2022-07-18 16:19:42 +0300

    Fix error: `add` did not work with 0 numbers

--- pkg/finaplan/add.go
+++ pkg/finaplan/add.go
@@ -1,12 +1,12 @@
 package finaplan
 
 func (p FinancialPlan) Add(amount ProjectionUnit, each uint64, start uint64) {
-	maxIndex := uint64(len(p.Projection)) - 1
-	if start > maxIndex {
+	var maxIndex int = len(p.Projection) - 1
+	if int(start) > maxIndex {
 		return
 	}
 
-	for i := start; i <= maxIndex; i++ {
+	for i := start; i <= uint64(maxIndex); i++ {
 		if each >= 1 {
 			intervalsPassed := (i - start) / each
 			p.Projection[i] += amount * ProjectionUnit(intervalsPassed+1)
