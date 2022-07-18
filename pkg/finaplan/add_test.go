0fa1bff4060fc613871b73b69c5e0d4c4c852123
Vadim Saprykin <sprkweb@ya.ru>
2022-07-18 15:54:48 +0300

    Fix tests for `add`

--- pkg/finaplan/add_test.go
+++ pkg/finaplan/add_test.go
@@ -8,9 +8,9 @@ func TestAdd(t *testing.T) {
 	plan := Init(DefaultConfig(), 5)
 	plan.Add(300, 2, 0)
 	expectedProjection := []float64{300, 300, 600, 600, 900}
-	for i, amount := range expectedProjection {
-		if amount != expectedProjection[i] {
-			t.Errorf("ELement №%d = %v does not match the expected value (%v)", i, amount, expectedProjection)
+	for i, amount := range plan.Projection {
+		if float64(amount) != expectedProjection[i] {
+			t.Errorf("Element №%d = %v does not match the expected value (%v)", i, amount, expectedProjection[i])
 		}
 	}
 }
@@ -19,9 +19,9 @@ func TestAddOnce(t *testing.T) {
 	plan := Init(DefaultConfig(), 6)
 	plan.Add(12.3, 0, 2)
 	expectedProjection := []float64{0, 0, 12.3, 12.3, 12.3, 12.3}
-	for i, amount := range expectedProjection {
-		if amount != expectedProjection[i] {
-			t.Errorf("ELement №%d = %v does not match the expected value (%v)", i, amount, expectedProjection)
+	for i, amount := range plan.Projection {
+		if float64(amount) != expectedProjection[i] {
+			t.Errorf("Element №%d = %v does not match the expected value (%v)", i, amount, expectedProjection[i])
 		}
 	}
 }
